17db26cd78915c3cb9ef2266477155902e9e6f08
Erik Prøis Olsnes <erik.prois.olsnes@nav.no>
2022-07-15 17:52:34 +0200

    YSMOD-444 Oppdatert template-navn med ny navngivning

--- saksbehandling/src/main/kotlin/no/nav/yrkesskade/saksbehandling/model/pdf/Pdf.kt
+++ saksbehandling/src/main/kotlin/no/nav/yrkesskade/saksbehandling/model/pdf/Pdf.kt
@@ -3,6 +3,6 @@ package no.nav.yrkesskade.saksbehandling.model.pdf
 data class PdfData(val brevtype: String, val uuid: String)
 
 enum class PdfTemplate(val templatenavn: String) {
-    TANNLEGEERKLAERING_VEILEDNING("tannlegeerklaering-veiledning"),
-    ARBEIDSTILSYNET_KOPI_VEILEDNING("arbeidstilsynet-kopi-veiledning")
+    VEILEDNINGSBREV_TANNLEGEERKLAERING("veiledningsbrev-tannlegeerklaering"),
+    VEILEDNINGSBREV_ARBEIDSTILSYNET_KOPI("veiledningsbrev-arbeidstilsynet-kopi")
 }
