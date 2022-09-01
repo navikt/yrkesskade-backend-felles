d4d6286ca15bb1f6f2ee5396a6645a3fb879dff0
Jone Lura <jone.lura@gmail.com>
2022-09-01 19:09:30 +0200

    YSMOD-523
    - utvidet PDF brev modell

--- saksbehandling/src/main/kotlin/no/nav/yrkesskade/saksbehandling/model/pdf/Pdf.kt
+++ saksbehandling/src/main/kotlin/no/nav/yrkesskade/saksbehandling/model/pdf/Pdf.kt
@@ -1,6 +1,14 @@
 package no.nav.yrkesskade.saksbehandling.model.pdf
 
-data class PdfData(val brevtype: String, val uuid: String)
+data class PdfData(val brevtype: String, val uuid: String, val innhold: List<PdfInnholdElement>)
+
+data class PdfInnholdElement(
+    val type: String,
+    val children: List<PdfTekstElement>,
+    val align: String
+)
+
+data class PdfTekstElement(val text: String)
 
 enum class PdfTemplate(val templatenavn: String) {
     VEILEDNINGSBREV_TANNLEGEERKLAERING("veiledningsbrev-tannlegeerklaering"),
