170d0a7c2f9d3e0c29df0dd873686c1f95cc95d7
Erik Prøis Olsnes <erik.prois.olsnes@nav.no>
2022-10-06 12:04:11 +0200

    YSMOD-646 Fjerne unødvendige felter fra brev/pdf-klasser

--- saksbehandling/src/main/kotlin/no/nav/yrkesskade/saksbehandling/model/Brev.kt
+++ saksbehandling/src/main/kotlin/no/nav/yrkesskade/saksbehandling/model/Brev.kt
@@ -1,15 +1,11 @@
 package no.nav.yrkesskade.saksbehandling.model
 
-import no.nav.yrkesskade.saksbehandling.model.pdf.PdfData
-import no.nav.yrkesskade.saksbehandling.model.pdf.PdfTemplate
+import no.nav.yrkesskade.saksbehandling.model.pdf.PdfInnholdElement
 import java.time.Instant
 
 data class Brev(
     val tittel: String,
-    val brevkode: String,
-    val enhet: String,
-    val template: PdfTemplate,
-    val innhold: PdfData,
+    val innhold: List<PdfInnholdElement>,
 )
 
 data class BrevutsendingBestiltHendelse(
--- saksbehandling/src/main/kotlin/no/nav/yrkesskade/saksbehandling/model/pdf/Pdf.kt
+++ saksbehandling/src/main/kotlin/no/nav/yrkesskade/saksbehandling/model/pdf/Pdf.kt
@@ -1,7 +1,5 @@
 package no.nav.yrkesskade.saksbehandling.model.pdf
 
-data class PdfData(val brevtype: String, val uuid: String, val innhold: List<PdfInnholdElement>)
-
 data class PdfInnholdElement(
     val type: String,
     val children: List<PdfTekstElement>,
@@ -9,8 +7,3 @@ data class PdfInnholdElement(
 )
 
 data class PdfTekstElement(val text: String)
-
-enum class PdfTemplate(val templatenavn: String) {
-    VEILEDNINGSBREV_TANNLEGEERKLAERING("veiledningsbrev-tannlegeerklaering"),
-    VEILEDNINGSBREV_ARBEIDSTILSYNET_KOPI("veiledningsbrev-arbeidstilsynet-kopi")
-}
